b29f4e4e9e2be9541f6b708f782887e779203147
XMKSOOR <XXALMSAFERXX@GMAIL.COM>
2026-06-02 19:29:44 +0300

    fix: complete auth rewrite - self-contained index.html, remove analytics CDN, safe firebase init, addEventListener, fix all pages auth flow

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyACPGcCa9V0IhjMJASYehP6NBR45IZ63Fw",
-  authDomain: "edu-classroom-a2b8d.firebaseapp.com",
-  projectId: "edu-classroom-a2b8d",
-  storageBucket: "edu-classroom-a2b8d.firebasestorage.app",
-  messagingSenderId: "1026113862086",
-  appId: "1:1026113862086:web:1b5a7f0e09841987d7dac7",
-  measurementId: "G-6ZH7JPQMF3"
-};
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+if(!firebase.apps.length){
+  firebase.initializeApp({
+    apiKey: "AIzaSyACPGcCa9V0IhjMJASYehP6NBR45IZ63Fw",
+    authDomain: "edu-classroom-a2b8d.firebaseapp.com",
+    projectId: "edu-classroom-a2b8d",
+    storageBucket: "edu-classroom-a2b8d.firebasestorage.app",
+    messagingSenderId: "1026113862086",
+    appId: "1:1026113862086:web:1b5a7f0e09841987d7dac7"
+  });
+}
+var auth = firebase.auth();
+var db = firebase.firestore();
+var storage = firebase.storage();
